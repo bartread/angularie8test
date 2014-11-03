65cd0a8e725b24bca298bee30ce6def44cffdcb9
Bart Read <bart.read@red-gate.com>
2014-11-03 13:48:42 +0000

    Added nested directives for yes/no questions.

--- scripts/test-directives.js
+++ scripts/test-directives.js
@@ -5,16 +5,44 @@ var testDirectivesApp = angular.module("testDirectives", []);
 testDirectivesApp.controller(
 	"testDirectivesCtrl",
 	["$scope", "$http", "$log", function($scope, $http, $log) {
-
+		$scope.selection="yes"
 	}]);
 
-testDirectivesApp.directive('displayInput', function() {
+testDirectivesApp.directive("displayInput", function() {
 	return {
 		scope: {
-			userInput: "=userInput"
+			userInput: "=userInput",
 		},
 		restrict: "E",
 		replace: "true",
 		templateUrl: "../views/directives/display-input.html"
 	};
-})
+});
+
+testDirectivesApp.directive("radioButton", function() {
+	return {
+		scope: {
+			id: "@id",
+			name: "@name",
+			value: "@value",
+			caption: "@caption",
+			selection: "=selection"
+		},
+		restrict: "E",
+		replace: "true",
+		templateUrl: "../views/directives/radio-button.html"
+	};
+});
+
+testDirectivesApp.directive("yesNoQuestion", function() {
+	return {
+		scope: {
+			questionId: "@questionId",
+			questionText: "@questionText",
+			selection: "=selection"
+		},
+		restrict: "E",
+		replace: "true",
+		templateUrl: "../views/directives/yes-no-question.html"
+	};
+});
