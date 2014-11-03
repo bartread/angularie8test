690db84a48b97964c1704bea6d299e8f27570202
Bart Read <bart.read@red-gate.com>
2014-11-03 09:59:30 +0000

    Initial commit for directives experimentation. Fixed weird injector error so we now have something very basic that works, but which doesn't yet use custom directives.

---
+++ scripts/test-directives.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var testDirectivesApp = angular.module("testDirectives", []);
+
+testDirectivesApp.controller(
+	"testDirectivesCtrl",
+	["$scope", "$http", "$log", function($scope, $http, $log) {
+
+	}]);
